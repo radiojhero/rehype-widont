e95e772b7baab833f89ef0d26571f33356a0ba77
Armando Meziat <amf-commits@mezi.at>
2021-01-10 20:08:45 -0300

    fix: redo index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,14 @@
-declare interface WidontOptions {
-  // Selector to match HTML tags to look into.
-  selector: string;
+import type { Attacher } from "unified";
+
+declare namespace widont {
+  interface WidontOptions {
+    /**
+     * Selector to match HTML tags to look into.
+     *
+     * @default "p, h1, h2, h3, h4, h5, h6, li, dt, dd"
+     */
+    selector: string;
+  }
 }
-
-declare const widont: Attacher<[WidontOptions?]>;
-
-export default widont;
-export { WidontOptions };
+declare const widont: Attacher<[widont.WidontOptions?]>;
+export = widont;
